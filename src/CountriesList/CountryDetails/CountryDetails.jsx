f5c116c4891bcc80c17533fc2fae83354afb79c2
Gapsa922 <goncalo_dsilva@hotmail.com>
2023-01-18 22:01:14 +0000

    added a banner

--- src/CountriesList/CountryDetails/CountryDetails.jsx
+++ src/CountriesList/CountryDetails/CountryDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
+import Banner from "../../Banner/Banner";
 
 const CountryDetails = () => {
 
@@ -24,6 +25,7 @@ const [showDescription, setShowDescription] = useState(false)
 
   return (
     <>
+      <Banner />
       <Link to="/countries-list"><button type="button">Back</button></Link>
       <h1>Country Details</h1>
     {/* {console.log(flag)} */}
@@ -34,7 +36,7 @@ const [showDescription, setShowDescription] = useState(false)
       </div>
       <div>
         {showDescription && 
-              <div>
+          <div>
               <p>People: {demonym}</p>
               <p>Region: {region}</p>
               <p>Population: {population}</p>
@@ -52,11 +54,8 @@ const [showDescription, setShowDescription] = useState(false)
                 {borders && borders.map((border, b) => (<p key={b.name}>{border}</p>))}
               </div>
             </div> }
-            <button type="button" onClick={() => setShowDescription(!showDescription)}>Click to see more</button>
-
+            <button type="button" onClick={() => setShowDescription(!showDescription)}>{!showDescription ? 'Click to see more' : 'Click to hide'}</button>
       </div>
-
-
     </>
   )
 }
